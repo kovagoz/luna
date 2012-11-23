cbc41d8553cda551cc6bcc8444e631fea2b43c10
Zoltan Kovago <kovago@gmail.com>
2012-11-23 20:47:28 +0100

    fix #1 and #2

--- public/index.php
+++ public/index.php
@@ -16,20 +16,31 @@ try {
     );
     if ($result === false) {
         throw new DomainException();
-    }  
-    $request->query->add($result['params']);
-    $request->attributes->set('target', $result['target']);
+    }
 
     // Dispatch
-    $response->setContent(
-        call_user_func(function() use ($app) {
-            return include sprintf(
-                '%s/controllers/%s.php',
-                APP_PATH,
-                $app['request']->attributes->get('target')
-            );  
-        })  
-    );
+    if (strstr($result['target'], '://') !== false) {
+        header('Location: ' . $result['target']);
+        exit();
+    } else if (substr($result['target'], -5) == '.html') {
+        $response->setContent($app['view']->render(
+            $result['target'],
+            $result['params']
+        ));
+    } else {
+        $request->query->add($result['params']);
+        $request->attributes->set('target', $result['target']);
+
+        $response->setContent(
+            call_user_func(function() use ($app) {
+                return include sprintf(
+                    '%s/controllers/%s.php',
+                    APP_PATH,
+                    $app['request']->attributes->get('target')
+                );  
+            })  
+        );
+    }
 } catch (DomainException $e) {
     $response->setStatusCode(404);
     $response->setContent('Page not found.');
